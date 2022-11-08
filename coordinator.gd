a025b94f3a615019b8450d4c4042c9db2c314060
Sarah Schulte <sarah.schulte@samsara.com>
2022-11-08 11:13:19 -0800

    actual determinism

--- coordinator.gd
+++ coordinator.gd
@@ -4,10 +4,18 @@ extends Spatial
 func _ready():
 	pass
 
+class MyCustomSorter:
+	static func sort_deterministic(a, b):
+		if a.get_index() < b.get_index():
+			return true
+		return false
+		
 func _process(delta):
 	if Input.is_action_just_pressed("ui_accept"):
 		for i in 2:
-			for pathfinder in get_tree().get_nodes_in_group("pathfinders"):
+			var pathfinders = get_tree().get_nodes_in_group("pathfinders")
+			pathfinders.sort_custom(MyCustomSorter, "sort_deterministic")
+			for pathfinder in pathfinders:
 				pathfinder.take_step()
 			yield(get_tree().create_timer(0.2), "timeout")
 			for monster in get_tree().get_nodes_in_group("monsters"):
