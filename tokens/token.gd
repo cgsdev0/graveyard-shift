fcf40a11e56b0d88f731ba13e605c0fdc124d7d7
Sarah Schulte <sarah.schulte@samsara.com>
2022-11-14 03:42:23 -0800

    token generator works now

--- tokens/token.gd
+++ tokens/token.gd
@@ -1,22 +1,18 @@
 class_name Token
-extends Spatial
+extends MeshInstance
 
 var width = 0.2
 export var scale_factor = 120.0
 export var albedo_color = Color.white
 export var sprite_texture = preload("res://debug/rbetta.png")
-export var sprite_mesh = preload("res://debug/rbetta_mesh.tres")
 
 func _ready():	
-	var mi = MeshInstance.new()
-	mi.mesh = sprite_mesh
 	var mat = SpatialMaterial.new()
 	mat.albedo_color = self.albedo_color
 	mat.set_texture(SpatialMaterial.TEXTURE_ALBEDO, sprite_texture)
 	mat.params_cull_mode = SpatialMaterial.CULL_DISABLED
-	mi.set_surface_material(0, mat)
-	mi.set_surface_material(1, mat)
-	mi.set_surface_material(2, mat)
-	mi.scale *= -1
-	mi.scale.x = 1
-	self.add_child(mi)
+	set_surface_material(0, mat)
+	set_surface_material(1, mat)
+	set_surface_material(2, mat)
+	scale *= -1
+	scale.x = 1
--- tokens/token_generator.gd
+++ tokens/token_generator.gd
@@ -7,13 +7,20 @@ export var scale_factor = 100.0
 var mi
 
 var points = []
-	
+var p
+
+func to_vec3(v2):
+	return Vector3(v2.x, v2.y, 0)
 func _ready():	
 	if get_tree().root.get_child(get_tree().root.get_child_count() - 1) != self:
 		print("NOPE")
 		return
-	mi = MeshInstance.new()
+		
+	$Control/FileOpen.popup()
+	var path = yield($Control/FileOpen, "file_selected")
+	$Sprite.texture = load(path)
 	
+	mi = MeshInstance.new()
 	var image = Image.new()
 	image.load($Sprite.texture.resource_path)
 
@@ -21,23 +28,29 @@ func _ready():
 	bitmap.create_from_image_alpha(image)
 	var polygons = bitmap.opaque_to_polygons(Rect2(Vector2(0, 0), bitmap.get_size()))
 	
-	var p = polygons[0]
+	p = []
+	print(polygons.size())
+	for point in polygons[0]:
+		p.push_back(point - $Sprite.texture.get_size() / 2)
+		
 	var triangulated = Geometry.triangulate_polygon(p)
 	for t in triangulated:
 		points.push_back(p[t])
 	var tool2d = SurfaceTool.new()
 	tool2d.begin(Mesh.PRIMITIVE_TRIANGLES)
 	for point in points:
-		tool2d.add_vertex(Vector3(point.x, point.y, 0))
+		tool2d.add_vertex(to_vec3(point))
 	var arr_mesh = tool2d.commit()
 	print(arr_mesh)
 	var mi2d = MeshInstance2D.new()
+	mi2d.modulate = Color.yellow
+	mi2d.modulate.a = 0.4
 	mi2d.mesh = arr_mesh
 	$Sprite.add_child(mi2d)
 	update()
 	mi.mesh = mi2d.mesh.duplicate()
 	
-	var outline = mi.mesh.create_outline(0).surface_get_arrays(0)[0]
+	var outline = p
 	outline.push_back(outline[0])
 	var mdt = MeshDataTool.new()
 	mdt.create_from_surface(mi.mesh, 0)
@@ -45,6 +58,7 @@ func _ready():
 	for i in range(mdt.get_vertex_count()):
 		var vertex = mdt.get_vertex(i)
 		mdt.set_vertex(i, vertex / scale_factor)
+		mdt.set_vertex_uv(i, Vector2(vertex.x / $Sprite.texture.get_width() + 0.5, vertex.y  / $Sprite.texture.get_height() + 0.5))
 	
 	mi.mesh.surface_remove(0)
 	mdt.commit_to_surface(mi.mesh)
@@ -60,24 +74,34 @@ func _ready():
 	for i in range(outline.size()):
 		if i + 1 >= outline.size():
 			break
-		st.add_vertex(outline[i] / scale_factor)
-		st.add_vertex(outline[i] / scale_factor + Vector3(0, 0, width))
-		st.add_vertex(outline[i + 1] / scale_factor)
-		st.add_vertex(outline[i] / scale_factor + Vector3(0, 0, width))
-		st.add_vertex(outline[i + 1] / scale_factor + Vector3(0, 0, width))
-		st.add_vertex(outline[i + 1] / scale_factor)
+		st.add_vertex(to_vec3(outline[i]) / scale_factor)
+		st.add_vertex(to_vec3(outline[i]) / scale_factor + Vector3(0, 0, width))
+		st.add_vertex(to_vec3(outline[i + 1]) / scale_factor)
+		st.add_vertex(to_vec3(outline[i]) / scale_factor + Vector3(0, 0, width))
+		st.add_vertex(to_vec3(outline[i + 1]) / scale_factor + Vector3(0, 0, width))
+		st.add_vertex(to_vec3(outline[i + 1]) / scale_factor)
 	st.generate_normals()
 	st.commit(mi.mesh)
 
 	mi.scale *= -1
 	mi.scale.x = 1
-	$Control/FileDialog.popup()
+	var splitted = path.rsplit("/", true, 1)
+	$Control/FileSave.set_current_file(splitted[1].replace(".png", "_mesh.tres"))
+	$Control/FileSave.popup()
+	$Control/FileSave.set_current_dir(splitted[0])
+	$Control/FileSave.deselect_items()
 	
 
 
 func _draw():
-	for i in points.size() - 1:
-		draw_line(points[i] + $Sprite.position, points[i+1] + $Sprite.position, Color.green)
+	if !p || !points:
+		return
+	for i in range(0, points.size(), 3):
+		draw_line(points[i] + $Sprite.position, points[i+1] + $Sprite.position, Color.red)
+		draw_line(points[i+1] + $Sprite.position, points[i+2] + $Sprite.position, Color.red)
+		draw_line(points[i] + $Sprite.position, points[i+2] + $Sprite.position, Color.red)
+	for i in p.size() - 1:
+		draw_line(p[i] + $Sprite.position, p[i+1] + $Sprite.position, Color.green)
 		pass
 
 
