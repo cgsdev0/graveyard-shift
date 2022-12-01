9c50645360ce6258a73cb6334a4a2c6c1adb5688
Sarah Schulte <sarah.schulte@samsara.com>
2022-12-01 05:11:55 -0800

    lure token

--- tokens/lure.gd
+++ tokens/lure.gd
@@ -2,6 +2,8 @@ extends Token
 
 var lure_range
 
+var lure2 = preload("res://textures/tokens/lure_2.png")
+
 func _ready():
 	add_to_group("lures")
 	var start_tween = Tween.new()
@@ -10,3 +12,8 @@ func _ready():
 	start_tween.interpolate_property(self, "scale", scale, Vector3(1,1,1),
 	1.1, Tween.TRANS_ELASTIC, Tween.EASE_OUT)
 	start_tween.start()
+	set_tex()
+
+func set_tex():
+	if lure_range > 100:
+		$token/Token.get_surface_material(2).albedo_texture = lure2
